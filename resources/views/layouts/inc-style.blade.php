f908c63835a2f945ae323bd3cedeb77911d68b91
kimkundad <kim.kundad@gmail.com>
2021-10-14 01:43:13 +0700

    kim

--- resources/views/layouts/inc-style.blade.php
+++ resources/views/layouts/inc-style.blade.php
@@ -166,18 +166,18 @@ font-size: 24px;
 .f_s_title_kim{
     font-size:18px;
 }
-@media (max-width: max-width:320){
+@media (max-width:320){
     .set_img_kim{
     height:100px;
 }
 }
-@media (max-width: max-width:414){
+@media (max-width::414){
     .set_img_kim{
     height:140px;
 }
 }
 
-@media only screen and (max-width:768px){
+@media (max-width:768px){
     .f_s_title_kim{
     font-size:12px;
 }
